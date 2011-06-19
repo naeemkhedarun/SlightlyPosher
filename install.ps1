a1a31ae868eb640aab03be23217746287c74266f
Naeem Khedarun <naeem.khedarun@gmail.com>
2011-06-19 23:11:57 +0100

    Added messages to install script.

--- install.ps1
+++ install.ps1
@@ -5,14 +5,21 @@ if($hasProfile -eq $false)
 	New-Item $profileDirectory -type directory
 	
 	New-Item $PROFILE -type file
+	"Creating profile."
 }
 
 $root = gl
 
-$isInstalled = Get-Content $PROFILE | ForEach-Object { if($_.Contains("$root\posh\profile.ps1") -eq $true){$true;break;}}
+$isInstalled = Get-Content $PROFILE | ForEach-Object { if($_.Contains("$root\posh\profile.ps1") -eq $true){$true;}}
 
 if($isInstalled -ne $true){
 
 	Add-Content $PROFILE "$root\posh\profile.ps1"
 	Add-Content $PROFILE ('$env:Path += ";' + $root + '\bin"')
+	
+	"Your environment has been configured at: " + $PROFILE
+}
+else
+{
+	Write-Host "Your environment is already configured at: " + $PROFILE
 }
