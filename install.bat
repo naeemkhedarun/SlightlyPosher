26ada6cb577529d554cd10da1def90901ba37bb4
Naeem Khedarun <naeem.khedarun@gmail.com>
2011-06-19 19:26:10 +0100

    Added simple profile installer.

---
+++ install.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell -NoProfile -ExecutionPolicy unrestricted -Command "& '.\install.ps1' %*"
+pause
---
+++ install.ps1
@@ -0,0 +1,12 @@
+﻿$hasProfile = Test-Path $PROFILE
+if($hasProfile -eq $false)
+{
+	$profileDirectory = Split-Path $PROFILE -Parent
+	New-Item $profileDirectory -type directory
+	
+	New-Item $PROFILE -type file
+}
+
+$root = gl
+Add-Content $PROFILE "$root\posh\profile.ps1"
+Add-Content $PROFILE ('$env:Path += ";' + $root + '\bin"')
